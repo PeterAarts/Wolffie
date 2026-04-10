1531b968272a09094663300834d64ac1fe4d19cb
Peter Aarts <21257547+PeterAarts@users.noreply.github.com>
2026-04-10 15:32:03 +0200

    Add files via upload

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -29,8 +29,8 @@ export default defineConfig({
         scope: '/',
         icons: [
           { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
-          { src: 'assets/woffie.svg', sizes: 'any', type: 'image/svg+xml' }
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+          { src: 'pwa-512x512-maskable.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ]
       }
     })
